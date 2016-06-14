5a5f0637b4bf88a3000289ee1ce36f94e193f751
Bruno Costa <netbofia@hotmail.com>
2016-06-14 14:28:37 +0100

    Including source for fasta_extractor, instead of pointer.

--- fasta_extractor/README.md
+++ fasta_extractor/README.md
@@ -1,1 +1,18 @@
-/home/brunocosta/.Software/Fasta_extractor/README.md
+# Fasta_extractor
+Author: Bruno Costa
+email: brunovasquescosta@gmail.com
+
+
+Fasta Sequence extractor
+
+Collection of scripts to extract sequences from fasta files
+
+<b>extract_x_seqs.sh</b>
+<u>Description</u>: Extracts x consequetive sequences with the given offset
+
+
+<u>Input</u>: [Fasta file][offset][n_sequences]
+
+<u>Output</u>: Standard output
+
+
--- fasta_extractor/extract_x_seqs.sh
+++ fasta_extractor/extract_x_seqs.sh
@@ -1,1 +1,39 @@
-/home/brunocosta/.Software/Fasta_extractor/extract_x_seqs.sh
+#!/usr/bin/env bash
+
+# extract_x_seqs.sh
+# brunovasquescosta@gmail.com
+# 
+# Modified by Bruno Costa on 01/06/2015
+# Copyright 2015 ITQB / UNL. All rights reserved.
+
+# Useful tool for extracting consecutive sequences from a fasta file 
+#
+# Call: extract_x_seqs.sh  [fasta] [start] [Num of Seqs]
+
+FILE=$1
+START=$2
+if [[ $START == 0 ]]; then
+    START=1
+fi
+N_SEQS=$(( $3 + $START )) 
+
+#Get dir of this script
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+awk -v N_SEQS=$N_SEQS -v START=$START '{ tmp = match($0, /^>.*/)      
+  if(tmp){
+    a++;
+  }      
+  if(tmp && a < N_SEQS && a >= START ){
+    print $0; newline;
+  }
+  if( tmp==false && a < N_SEQS && a >= START ){
+    print $0;
+  }        
+  if( a >= N_SEQS ){
+    exit 0
+  }
+}' ${FILE}
+
+exit 0 
+
